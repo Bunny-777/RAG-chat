7ca2f87a65b78b2b2d79d447a52721534f45be54
Bunny <bunny202410@gmail.com>
2026-06-25 21:43:51 +0530

    fixed all the minor bugs

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from youtube_transcript_api import YouTubeTranscriptApi, TranscriptsDisabled
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_groq import ChatGroq
 from langchain_community.vectorstores import FAISS
+from sentence_transformers import SentenceTransformer
 from langchain_core.prompts import PromptTemplate
+from langchain_huggingface import HuggingFaceEmbeddings
 from dotenv import load_dotenv
 
-
-
+load_dotenv()
 video_url="https://www.youtube.com/watch?v=kXB55AGmDPI"
 
 video_id = "kXB55AGmDPI" # only the ID, not full URL
@@ -25,10 +25,8 @@ splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
 chunks = splitter.create_documents([transcript])
 
 #generating embeddings
-embeddings = HuggingFaceEmbeddings(
-    model_name="sentence-transformers/all-MiniLM-L6-v2",
-    model_kwargs={'device': 'cpu'}
-)
+embeddings=HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
 #storing in vector store database
 vector_store = FAISS.from_documents(chunks, embeddings)
+print("chal rha he ")
