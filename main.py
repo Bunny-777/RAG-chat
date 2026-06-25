c7d86053b6e7a6b1d06643cedbe85ef4b5511bea
Bunny <bunny202410@gmail.com>
2026-06-25 13:04:18 +0530

    indexing done

--- main.py
+++ main.py
@@ -2,22 +2,21 @@ from youtube_transcript_api import YouTubeTranscriptApi, TranscriptsDisabled
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_groq import ChatGroq
-from langchain_community.vectorstores import FAISS
 from langchain_core.prompts import PromptTemplate
 from dotenv import load_dotenv
 
+
+
 video_url="https://www.youtube.com/watch?v=kXB55AGmDPI"
-video_id = "kXB55AGmDPI" # only the ID, not full URL
+
+video_id = "9tlsHKAoPkY" # only the ID, not full URL
 try:
     ytt_api = YouTubeTranscriptApi()
     fetched_transcript = ytt_api.fetch(video_id)
-    # print(type(fetched_transcript))
-    # Flatten it to plain text
-    for snippet in fetched_transcript:
-        print(snippet.text)
-    # transcript = " ".join(chunk["text"] for chunk in fetched_transcript)
-    # print(transcript)
-
+    transcript=""
+    for i in fetched_transcript:
+        transcript+=i.text
+    print(transcript)
 except TranscriptsDisabled:
     print("No captions available for this video.")
 
